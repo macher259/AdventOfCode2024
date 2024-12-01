92b92aed72fbfbc578003016b5660f663dbd1f56
Maciej Herdon <maciej.herdon@gmail.com>
2024-12-01 14:21:38 +0100

    chore: Init commit

---
+++ src/main/java/com/macher259/aoc2024/Main.java
@@ -0,0 +1,25 @@
+package com.macher259.aoc2024;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException {
+        exec(1);
+    }
+
+    private static void exec(int num) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("day" + num + ".in")) {
+            var data = new String(input.readAllBytes());
+            var day = Class.forName("com.macher259.aoc2024.Day" + num);
+            var obj = day.getDeclaredConstructor(String.class).newInstance(data);
+
+            for (int i = 1; i <= 2; i++) {
+                var method = day.getDeclaredMethod("part" + i);
+                var result = method.invoke(obj);
+                System.out.println("Day " + num + " part " + i + ": " + result);
+            }
+        }
+    }
+}
